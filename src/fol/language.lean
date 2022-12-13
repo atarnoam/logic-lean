164e1afde76522cd5368914b562696506961ba29
Noam Atar <atarnoam@gmail.com>
2022-12-13 20:19:56 +0200

    started working on semantics

--- src/fol/language.lean
+++ src/fol/language.lean
@@ -12,29 +12,29 @@ noncomputable theory
 
 -- Sadly, lean does not support good inductive types.
 -- Hence we won't have arbitrary arity functions and relations.
-structure signature : Type :=
+structure Signature : Type :=
 (consts : ℕ)
 (funcs : ℕ)
 (ops : ℕ)
 (rels : ℕ)
 
 @[derive [decidable_eq]]
-structure Const (S : signature) : Type :=
+structure Const (S : Signature) : Type :=
 (const : ℕ)
 (const_le : const < S.consts)
 
 @[derive [decidable_eq]]
-structure Func (S : signature) : Type := 
+structure Func (S : Signature) : Type := 
 (func : ℕ)
 (func_le : func < S.funcs)
 
 @[derive [decidable_eq]]
-structure Op (S : signature) : Type := 
+structure Op (S : Signature) : Type := 
 (op : ℕ)
 (op_le : op < S.ops)
 
 @[derive [decidable_eq]]
-structure Rel (S : signature) : Type := 
+structure Rel (S : Signature) : Type := 
 (rel : ℕ)
 (rel_le : rel < S.rels)
 
@@ -42,16 +42,28 @@ structure Rel (S : signature) : Type :=
 def var := ℕ
 
 @[derive [decidable_eq]]
-inductive Term (S : signature) : Type
+inductive Term (S : Signature) : Type
 | Var : var → Term
 | ConstT : Const S → Term
 | FuncT : Func S → Term → Term   
 | OpT : Op S → Term → Term → Term
 
-instance {S : signature} : inhabited (Term S) :=
+instance var_has_coe {S : Signature} : has_coe var (Term S) :=
+⟨λ v, Term.Var v⟩
+
+instance const_has_coe {S : Signature} : has_coe (Const S) (Term S) :=
+⟨λ c, Term.ConstT c⟩
+
+instance func_has_coe {S : Signature} : has_coe_to_fun (Func S) (λ _, Term S → Term S) :=
+⟨λ f, Term.FuncT f⟩
+
+instance op_has_coe {S : Signature} : has_coe_to_fun (Op S) (λ _, Term S → Term S → Term S) :=
+⟨λ o, Term.OpT o⟩
+
+instance {S : Signature} : inhabited (Term S) :=
 ⟨Term.Var 0⟩
 
-def vars (S : signature) : Term S → set var := 
+def vars (S : Signature) : Term S → set var := 
 λ t, begin
   induction t with v c f t st o t₁ t₂ st₁ st₂,
   exact {v},
@@ -69,17 +81,17 @@ inductive Quantifier : Type
 | All : Quantifier
 
 @[derive [decidable_eq]]
-inductive Formula (S : signature) : Type
+inductive Formula (S : Signature) : Type
 | Eq : Term S → Term S → Formula
 | RelF : Rel S → Term S → Term S → Formula
 | Not : Formula → Formula
 | OpL  : LogicalOp → Formula → Formula → Formula
 | QuantifierL : Quantifier → var → Formula → Formula 
 
-instance {S : signature} : inhabited (Formula S) :=
+instance {S : Signature} : inhabited (Formula S) :=
 ⟨Formula.Eq (Term.Var 0) (Term.Var 0)⟩
 
-def free (S : signature) : Formula S → set var :=
+def free (S : Signature) : Formula S → set var :=
 λ φ, 
 begin
   induction φ with t₁ t₂ r t₁ t₂ φ sφ o φ₁ φ₂ sφ₁ sφ₂ q v φ sφ,
@@ -90,11 +102,11 @@ begin
   exact sφ \ {v},
 end
 
-def sentence {S : signature} (φ : Formula S) : Prop := free S φ = ∅ 
+def sentence {S : Signature} (φ : Formula S) : Prop := free S φ = ∅ 
 
 namespace language
 
-variable {S : signature}
+variable {S : Signature}
 
 section term
 
@@ -139,4 +151,4 @@ end free
 end language
 
 -- @[derive [has_mem, has_singleton Formula, has_union, has_emptyc, has_subset, complete_lattice]]
-def Theory (S : signature) := set (Formula S)
+def Theory (S : Signature) := set (Formula S)
--- src/fol/semantics.lean
+++ src/fol/semantics.lean
@@ -4,17 +4,73 @@ open language
 
 universe u
 
-structure Structure (S : signature) :=
-(Dom : Type u)
-(const : Const S → Dom)
-(func : Func S → Dom → Dom)
-(op : Op S → Dom → Dom → Dom)
-(rel : Rel S → Dom → Dom → Prop)
+structure Structure (S : Signature) :=
+(dom : Type u)
+(const : Const S → dom)
+(func : Func S → dom → dom)
+(op : Op S → dom → dom → dom)
+(rel : Rel S → dom → dom → Prop)
 
-variables {S : signature}
+instance {S : Signature} : has_coe_to_sort (Structure S) (Type u) := ⟨Structure.dom⟩
 
-def assignment (A : Structure S) := var → A.Dom
+variables {S : Signature}
 
-def subs_assignment {S : signature} {A : Structure S} 
-(β : assignment A) (a : A.Dom) (x : var) : assignment A :=
-λ v, ite (v = x) a (β v)
+structure assignment (A : Structure S) :=
+(ass : var → A)
+
+section assignment
+
+variable {A : Structure S}
+
+def subs_assignment (β : assignment A) (a : A) (x : var) : assignment A :=
+⟨λ v, ite (v = x) a (β.ass v)⟩ 
+
+notation _[_ / _] := subs_assignment
+
+def interp (β : assignment A) : Term S → A :=
+λ t,
+begin
+  induction t with v c f t a o t₁ t₂ a₁ a₂,
+  exact β.ass v,
+  exact A.const c,
+  exact A.func f a,
+  exact A.op o a₁ a₂
+end
+
+instance : has_coe_to_fun (assignment A) (λ _, Term S → A) :=
+⟨λ β, interp β⟩
+
+def eval : ∀ (β : assignment A) (φ : Formula S), Prop
+| β (Formula.Eq t₁ t₂) := β t₁ = β t₂ 
+| β (Formula.RelF R t₁ t₂) := A.rel R (β t₁) (β t₂)
+| β (Formula.Not φ) := ¬eval β φ
+| β (Formula.OpL LogicalOp.To φ₁ φ₂) := (eval β φ₁) → (eval β φ₂)
+| β (Formula.QuantifierL Quantifier.All v φ) := ∀ (a : A), eval (subs_assignment β a v) φ
+
+end assignment
+
+namespace semantics
+
+variable {A : Structure S}
+
+section interp
+
+lemma interp_var {β : assignment A} {v : var} : β v = β.ass v := rfl
+
+lemma interp_const {β : assignment A} {c : Const S} : β c = A.const c := rfl
+
+lemma interp_func {β : assignment A} {f : Func S} {t : Term S} : 
+interp β (f t) = A.func f (β t) := rfl
+
+lemma interp_op {β : assignment A} {o : Op S} {t₁ t₂ : Term S} :
+interp β (o t₁ t₂) = A.op o (β t₁) (β t₂) := rfl
+
+end interp
+
+section eval
+
+lemma eval_eq {β : assignment A} {t₁ t₂ : Term S} 
+
+end eval
+
+end semantics
